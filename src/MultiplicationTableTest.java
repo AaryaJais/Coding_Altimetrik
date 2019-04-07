21cf413e9dc0c5c0091212ec89ae9ef1638ba684
webappclient <webappclient@localhost.com>
2019-04-07 14:36:54 +0530

    Added code to print Multiplication table of first 10 prime number

---
+++ src/MultiplicationTableTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+public class MultiplicationTableTest {
+	
+    @Test
+	public void printMultiplicationOfPrimeTest()
+	{
+		MultiplicationTable mtTestInstance  = new MultiplicationTable();
+		mtTestInstance.printMultiplicationOfPrime(10);
+	}
+	
+	@Test(expected = Exception.class)
+	public void printMultiplicationOfPrimeTestNegativeValue()
+	{
+		MultiplicationTable mtTestInstance  = new MultiplicationTable();
+		mtTestInstance.printMultiplicationOfPrime(-3);
+	}
+	
+
+}
